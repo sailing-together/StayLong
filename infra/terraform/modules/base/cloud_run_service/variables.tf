a4bd262307c63e656ff55eb1d4bcabf4b095f5be
Yvonne Yao <yvonne.yao278@gmail.com>
2026-08-17 21:32:52 +1000

    fix: grant deployer Cloud Run invoke access

--- infra/terraform/modules/base/cloud_run_service/variables.tf
+++ infra/terraform/modules/base/cloud_run_service/variables.tf
@@ -3,6 +3,10 @@ variable "location" { type = string }
 variable "service_name" { type = string }
 variable "service_account_email" { type = string }
 variable "image" { type = string }
+variable "invoker_members" {
+  type    = set(string)
+  default = []
+}
 variable "deletion_protection" {
   type    = bool
   default = false
